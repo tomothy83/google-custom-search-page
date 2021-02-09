5de6ea34890a78445b03b0a6348aa767d68b3c50
Tomoaki Yasaka <34957299+tomothy83@users.noreply.github.com>
2021-02-09 14:15:03 +0900

    Query を表示

--- public/js/script.js
+++ public/js/script.js
@@ -4,16 +4,24 @@ $(document).ready(function () {
   const $addButtonArea = $('<div class="add-btn-area"></div>');
   $addButtonArea.append($addBtn);
 
-  myWebResultsRenderedCallback = function(){
+  let searchQuery = '';
+
+  const myWebResultsRenderedCallback = function(){
     $('.gsc-webResult').each(function(i, e) {
       $(e).append($addButtonArea.clone());
     });
   };
 
+  const mySearchStartingCallback = function(gname, query) {
+    searchQuery = query;
+    return query;
+  };
+
   window.__gcse || (window.__gcse = {});
   window.__gcse.searchCallbacks = {
     web: {
-      rendered: 'myWebResultsRenderedCallback',
+      starting: mySearchStartingCallback,
+      rendered: myWebResultsRenderedCallback
     },
   };
 
@@ -23,7 +31,30 @@ $(document).ready(function () {
     deferRender: true,
     columns: [
       {
+        data: 'query'
+      },
+      {
+        data: 'title'
+      },
+      {
         data: 'url',
+        render: function(data, type, row, meta) {
+          if (type !== 'display') {
+            return data;
+          }
+          let trimmed = data.substr(0, 27);
+          if (trimmed !== data) {
+            trimmed += '...';
+          }
+          return (
+            '<a href="' + data + '" target="_blank">'
+            + trimmed
+            + '</a>'
+          );
+        }
+      },
+      {
+        data: 'breadcrumbs',
         render: function(data, type, row, meta) {
           if (type !== 'display') {
             return data;
@@ -36,23 +67,36 @@ $(document).ready(function () {
         }
       },
       {
-        data: 'title'
+        data: 'article_date',
+        visible: false
       },
       {
-        data: 'breadcrumbs'
-      },
-      {
-        data: 'article_date'
+        data: 'snippet',
+        visible: false
       },
       {
         data: 'added_at',
         visible: false
-      },
-      {
-        data: 'snipet',
-        visible: false
       }
     ]
+  });
+
+  $('body').on('click', 'button.add-to-dt', function() {
+    const $result = $(this).closest('.gsc-webResult');
+    const title = $result.find('a.gs-title').first().text();
+    const url = $result.find('a.gs-title').first().attr('href');
+    const breadcrumbs = $result.find('.gs-visibleUrl.gs-visibleUrl-breadcrumb').first().text();
+    const snippet = $result.find('.gs-snippet').first().text();
+    dTable.row.add({
+      query: searchQuery,
+      title: title,
+      url: url,
+      breadcrumbs: breadcrumbs,
+      article_date: null,
+      snippet: snippet,
+      added_at: new Date()
+    });
+    dTable.columns.adjust().draw();
   });
 
 });
